b706f0b64c98fa815f08083879050634dfc117f7
Rodrigo Tardelli <rodrigotmg@gmail.com>
2020-08-24 15:47:57 -0300

    Used models comming from service inn detail page

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RecipesService } from '../recipes.service';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeDetailPage implements OnInit {
 
-  constructor() { }
+  loadedRecipe: Recipe;
+
+  constructor(private activatedRoute: ActivatedRoute, private recipesService: RecipesService) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      if(!paramMap.has('recipeId')){
+        // redirect
+        return;
+      }
+
+      const recipeId = paramMap.get('recipeId');
+      this.loadedRecipe = this.recipesService.getRecipe(recipeId);
+    });
   }
 
 }
